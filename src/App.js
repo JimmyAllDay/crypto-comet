044bd2e80ca15ec8ca99dc79ae4b1d834cf87af1
James Marshall <jameswhmarshallactions@gmail.com>
2021-10-27 22:14:55 +1100

    more comments

--- src/App.js
+++ src/App.js
@@ -77,10 +77,12 @@ export default App;
 // Build out tests, focussing on logic and use these in TDD for future updates
 // User is currently able to click on a link (for eg, page 6) in the pagination component, then enter a search criteria, and see no results,
 // despite that results are available in the data array - logic needs to be updated to take user back to last populated page on user filter of data array
-// the div with className 'spacer' should move into a component - this is unecessarily cluttering the App.js file though is behaving erratically when encolsed within a component.
+// Loading state does not make use of request size info from http request to show user data loading status through for eg a loading bar. Axios intalled to expose property on http response to allow this functionality, though server not returning value data. Correct headers may need to be sent manually.
+// Loader is under-developed. You may wish to update this with an npm package for React, or futher customise the styling on the existing component
+// The div with className 'spacer' should move into a component - this is unecessarily cluttering the App.js file though is behaving erratically when encolsed within a component. Style prop to be removed from App.js.
 // Repsonsive design needs to be updated for the coin and header components - d-none classes applied to relevant divs are not registering in the DOM
 // Address additional items in the spec - include new tab for most popular currencies - axios can be used to make simulatenous api calls
-// Build out additional component for each coin, that navigates to a detailed display for the coin, and shows a ticker for the price of the selected coin
+// Build out additional component for each coin - one that navigates to a detailed display for the coin, and shows a ticker for the price of the selected coin
 // Include 'favourites' component that allows users to push coins into a new array, where components display additional info, such as fluctutations over longer time-periods
 // Footer can be further developed
 // bakground image behaviour can be refined
